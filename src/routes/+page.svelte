787c5706ef1a3d76c66a48b9768c0d9472415405
Rudolf Hersén <rudolf.hersen@schibsted.com>
2022-09-12 11:50:55 +0200

    add colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <script>
 	export let data;
+
+	function color(price) {
+		if (price < 360) return 'green';
+		if (price > 460) return 'red';
+		return 'orange';
+	}
 </script>
 
 <h1>Elpris per timme {data.date}</h1>
 <ol>
 	{#each data.prices as { Value }}
-		<li>{Value}</li>
+		<li style="color: {color(Value)}">{Value}</li>
 	{/each}
 </ol>
