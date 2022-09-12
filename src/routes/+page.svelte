14e2f74ddcd550d5b7f321b097526654520b1f13
Rudolf Hersén <rudolf.hersen@schibsted.com>
2022-09-12 13:17:12 +0200

    dark mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,28 @@
 	export let data;
 
 	function color(price) {
-		if (price < 360) return 'green';
-		if (price > 460) return 'red';
-		return 'orange';
+		if (price < 360) return 'hsl(120deg 100% 65%)';
+		if (price > 460) return 'hsl(0deg 100% 65%)';
+		return 'hsl(60deg 100% 65%)';
 	}
 </script>
 
-<h1>Elpris per timme {data.date}</h1>
-<ol>
-	{#each data.prices as { Value }}
-		<li style="color: {color(Value)}">{Value}</li>
-	{/each}
-</ol>
+<div>
+	<h1>Elpris per timme {data.date}</h1>
+	<ol>
+		{#each data.prices as { Value }}
+			<li style="color: {color(Value)}">{Value}</li>
+		{/each}
+	</ol>
+</div>
+
+<style>
+	div {
+		background: black;
+		font-family: sans-serif;
+	}
+
+	h1 {
+		color: white;
+	}
+</style>
