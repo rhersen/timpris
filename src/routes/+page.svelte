34b5e83fd1464868ba8d9ee00f0dce108d555469
Rudolf Hersén <rudolf.hersen@schibsted.com>
2022-09-15 11:18:54 +0200

    use the client's time zone

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,25 @@
 <script>
 	import { color, current, today } from '$lib/color.js';
 	export let data;
+	export let prices = data.prices.map((price) => ({
+		value: price.Value,
+		date: new Date(Date.parse(price.TimeStamp))
+	}));
+
+	function pad(hours) {
+		return hours.toString().padStart(2, '0');
+	}
 </script>
 
 <div>
 	<h1>Elpris per timme</h1>
-	{#each data.prices as { TimeStamp, date, Value }}
+	{#each prices as { date, value }}
 		{#if date.getHours() === 0}
 			<h2>{today(date)}</h2>
 		{/if}
-		<div style="color: {color(Value, data.limits)}" class:current={current(date)}>
-			{date.getHours().toString().padStart(2, '0')}–{(date.getHours() + 1)
-				.toString()
-				.padStart(2, '0')}
-			{Value}
+		<div style="color: {color(value, data.limits)}" class:current={current(date)}>
+			{pad(date.getHours())}–{pad(date.getHours() + 1)}
+			{value}
 		</div>
 	{/each}
 </div>
