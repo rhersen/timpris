815a361df4691b3cef46929ff3177d81a7a319f3
Rudolf Hersén <rudolf.hersen@schibsted.com>
2022-10-07 08:36:13 +0200

    don't show yesterday

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -4,20 +4,17 @@ export async function load() {
 	const HOURS = 60 * 60 * 1000;
 	const dateTimeFormat = new Intl.DateTimeFormat('sv-SE', { dateStyle: 'short' });
 
+	const today = dateTimeFormat.format(Date.now());
+	const tomorrow = dateTimeFormat.format(Date.now() + 24 * HOURS);
+
 	if (!HOURS) {
 		const storumanResponse = await fetch(
-			`https://www.storumanenergi.se/net.seab.jsonproxy/spotprices.jsp?elarea=SE3&date=${dateTimeFormat.format(
-				Date.now()
-			)}`
+			`https://www.storumanenergi.se/net.seab.jsonproxy/spotprices.jsp?elarea=SE3&date=${today}`
 		);
-
 		return storuman(await storumanResponse.json());
 	}
-
 	const vattenfallResponse = await fetch(
-		`https://www.vattenfall.se/api/price/spot/pricearea/${dateTimeFormat.format(
-			Date.now() - 24 * HOURS
-		)}/${dateTimeFormat.format(Date.now() + 24 * HOURS)}/SN3`
+		`https://www.vattenfall.se/api/price/spot/pricearea/${today}/${tomorrow}/SN3`
 	);
 
 	return vattenfall(await vattenfallResponse.json());
