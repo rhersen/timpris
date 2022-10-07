e428579cb4a48ec266fbf36e00c192c1a34e0da2
Rudolf Hersén <rudolf.hersen@schibsted.com>
2022-10-07 10:16:42 +0200

    use storuman as primary and vattenfall as fallback

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,21 +1,31 @@
 import { storuman, vattenfall } from '$lib/transform.js';
 
-export async function load() {
-	const HOURS = 60 * 60 * 1000;
-	const dateTimeFormat = new Intl.DateTimeFormat('sv-SE', { dateStyle: 'short' });
+const HOURS = 60 * 60 * 1000;
+const dateTimeFormat = new Intl.DateTimeFormat('sv-SE', { dateStyle: 'short' });
 
+export async function load() {
 	const today = dateTimeFormat.format(Date.now());
 	const tomorrow = dateTimeFormat.format(Date.now() + 24 * HOURS);
 
-	if (HOURS) {
-		const storumanResponse = await fetch(
-			`https://www.storumanenergi.se/net.seab.jsonproxy/spotprices.jsp?elarea=SE3&date=${today}`
-		);
-		return storuman([await storumanResponse.json()]);
-	}
-	const vattenfallResponse = await fetch(
-		`https://www.vattenfall.se/api/price/spot/pricearea/${today}/${tomorrow}/SN3`
-	);
+	console.log('fetching storuman/tomorrow');
+	const tomorrowResponse = await fetch(storumanUrl(tomorrow));
 
-	return vattenfall(await vattenfallResponse.json());
+	if (tomorrowResponse.ok) {
+		console.log('storuman/tomorrow ok, fetching today');
+		const todayResponse = await fetch(storumanUrl(today));
+		return storuman(await Promise.all([todayResponse, tomorrowResponse].map((rsp) => rsp.json())));
+	}
+
+	console.log('fetching vattenfall');
+	const response = await fetch(vattenfallUrl(today, tomorrow));
+
+	return vattenfall(await response.json());
+}
+
+function storumanUrl(date) {
+	return `https://www.storumanenergi.se/net.seab.jsonproxy/spotprices.jsp?elarea=SE3&date=${date}`;
+}
+
+function vattenfallUrl(today, tomorrow) {
+	return `https://www.vattenfall.se/api/price/spot/pricearea/${today}/${tomorrow}/SN3`;
 }
